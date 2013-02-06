b24ea8365b2ec74de084cf211da8f427af661e41
GardenTree <takahiro.ueki@gmail.com>
2013-02-06 12:12:09 +0900

    Execute [rails new twirl -T]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twirl::Application.config.secret_token = '200494314c0c7da82c8f28fff614eb277239b08f3d7279c9548efbc683e37ec11e695c89774e618b52ee82541a379fd17ca612ce31593a16f42b0a99ac29bf11'
